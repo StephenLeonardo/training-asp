ad3162fc79c8bdfd69a8e25e487fb5d66cca79f4
pemoeda08 <13th.days@gmail.com>
2020-12-18 15:12:43 +0700

    trainiong

---
+++ WebApplication2/Controllers/BookController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication2.Models;
+
+namespace WebApplication2.Controllers
+{
+    public class BookController : Controller
+    {
+        // GET: Book
+        public ActionResult Index()
+        {
+
+            var book1 = new BookModel()
+            {
+                Judul = "Sang Pendekar"
+            };
+
+            return View("List", book1);
+        }
+
+        public class Dummy
+        {
+            public string Test { get; set; }
+        }
+
+    }
+
+
+
+}
