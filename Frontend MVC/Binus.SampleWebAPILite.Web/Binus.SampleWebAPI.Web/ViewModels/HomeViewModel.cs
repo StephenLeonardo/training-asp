b049f7bd8fc210a83abb993d70b5dbf5262ba30b
pemoeda08 <13th.days@gmail.com>
2020-12-21 10:55:07 +0700

    referensi model validation

--- Frontend MVC/Binus.SampleWebAPILite.Web/Binus.SampleWebAPI.Web/ViewModels/HomeViewModel.cs
+++ Frontend MVC/Binus.SampleWebAPILite.Web/Binus.SampleWebAPI.Web/ViewModels/HomeViewModel.cs
@@ -6,6 +6,7 @@ using System.Web;
 
 namespace Binus.SampleWebAPI.Web.ViewModels
 {
+    // https://docs.microsoft.com/en-us/aspnet/mvc/overview/getting-started/introduction/adding-validation -- Referensi Server-side validation
     public class HomeViewModel
     {
         public List<BookModel> Books { get; set; }
